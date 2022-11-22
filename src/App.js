94f100f944c477fbfd84f44029ed6b7efe21d29e
krst221 <krst221@gmail.com>
2022-11-22 21:01:52 +0100

    facebook login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import FacebookLogin from 'react-facebook-login';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { gapi } from 'gapi-script';
 import './App.css';
@@ -6,6 +7,11 @@ import './App.css';
 function App() {
   const [ profile, setProfile ] = useState([]);
   const clientId = '862987207402-6cpa0kbkkglpu55tgfkfn5b1lahvhb2q.apps.googleusercontent.com';
+  const [fbUser, setFbUser] = useState();
+
+  const responseFacebook = (response) => {
+    setFbUser(response);
+  }
 
   useEffect(() => {
     const initClient = () => {
@@ -27,31 +33,43 @@ function App() {
     setProfile(null);
   };  
 
+
+
   return (
     <div>
-    <h2>React Google Login</h2>
-    <br />
-    <br />
-    {profile ? (
-        <div>
-            <img src={profile.imageUrl} alt="user_image" />
-            <h3>User Logged in</h3>
-            <p>Name: {profile.name}</p>
-            <p>Email Address: {profile.email}</p>
-            <br />
-            <br />
-            <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
-        </div>
-    ) : (
-        <GoogleLogin
-            clientId={clientId}
-            buttonText="Sign in with Google"
-            onSuccess={onSuccess}
-            onFailure={onFailure}
-            cookiePolicy={'single_host_origin'}
-            isSignedIn={true}
-        />
-    )}
+      {fbUser ?
+        <div><h2>{fbUser.name}</h2><h3>{fbUser.email}</h3></div>
+       :
+        <FacebookLogin
+        appId="540373324605232"
+        autoLoad={true}
+        fields="name,email,picture"
+        callback={responseFacebook} /> 
+      }
+
+      <h2>React Google Login</h2>
+      <br />
+      <br />
+      {profile ? (
+          <div>
+              <img src={profile.imageUrl} alt="user_image" />
+              <h3>User Logged in</h3>
+              <p>Name: {profile.name}</p>
+              <p>Email Address: {profile.email}</p>
+              <br />
+              <br />
+              <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
+          </div>
+      ) : (
+          <GoogleLogin
+              clientId={clientId}
+              buttonText="Sign in with Google"
+              onSuccess={onSuccess}
+              onFailure={onFailure}
+              cookiePolicy={'single_host_origin'}
+              isSignedIn={true}
+          />
+      )}
 </div>
 );
 }

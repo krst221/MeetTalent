d981a0f915fe7a2c9bb1a0720dcf50aee2286d7a
krst221 <krst221@gmail.com>
2022-11-22 20:42:07 +0100

    login google

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
 import './App.css';
 
 function App() {
+  const [ profile, setProfile ] = useState([]);
+  const clientId = '862987207402-6cpa0kbkkglpu55tgfkfn5b1lahvhb2q.apps.googleusercontent.com';
+
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: ''
+      });
+    };
+    gapi.load('client:auth2', initClient);
+  });
+
+  const onSuccess = (res) => {
+    setProfile(res.profileObj);
+  };
+  const onFailure = (err) => {
+    console.log('failed:', err);
+  };
+  const logOut = () => {
+    setProfile(null);
+  };  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+    <h2>React Google Login</h2>
+    <br />
+    <br />
+    {profile ? (
+        <div>
+            <img src={profile.imageUrl} alt="user_image" />
+            <h3>User Logged in</h3>
+            <p>Name: {profile.name}</p>
+            <p>Email Address: {profile.email}</p>
+            <br />
+            <br />
+            <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
+        </div>
+    ) : (
+        <GoogleLogin
+            clientId={clientId}
+            buttonText="Sign in with Google"
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+            cookiePolicy={'single_host_origin'}
+            isSignedIn={true}
+        />
+    )}
+</div>
+);
 }
 
 export default App;
